4282c38e25f5f3f660cd0aed7efe38cd2c1cf45c
sneha <rosh3267@gmail.com>
2023-12-10 13:46:04 +0530

    Mod#2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2     # image processing
 import numpy as np # numerical processing
 from PIL import Image # image processing
+from PIL import ImageDraw, ImageFont
 import matplotlib.pyplot as plt # plotting 
 
 # Config
@@ -13,23 +14,56 @@ st.set_page_config(
 
 )
 
+with st.container():
+    st.title(':rainbow[Image Manipulation App]')
+    
+
+
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
-with st.spinner('Processing Immigration Data'):
+
+with st.spinner('Processing Image'):
     if uploaded_file is not None:
         # Convert the file to an opencv image.
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-
+        open_image=Image.open(uploaded_file)
+        
         #  let's display the image:
-        st.image(opencv_image, channels="BGR")
+        st.image(open_image,caption='Original Image')
 
+
+
+st.subheader('Resize the image',divider='blue')
+c1,c2=st.columns(2)
 #Resize
-if st.button('Resize'):
-    new_image = Image.open(uploaded_file)
-    ht = st.number_input('Enter the height of image: ')
-    wd = st.number_input('Enter the width of image: ')
-    new_image.resize((ht,wd))
+if c1.button('Resize'): 
+ ht = st.number_input('Enter the height of image: ',placeholder='Type a number',value=None)
+ wd = st.number_input('Enter the width of image: ', placeholder='Type a Number',value=None)
+ Resized=open_image.resize((ht,wd))
+ c2.image(Resized)
 
+
+#Color change
+st.subheader('Convert the color of the image',divider='rainbow')
+
+c1,c2=st.columns(2)
+if c1.button('Gray'):
+        im=cv2.imread(uploaded_file)
+        cats_color_grey=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        plt.imshow(cats_color_grey,cmap='gray')
+        plt.axis('off')
+
+
+st.subheader('Rotate the image',divider='blue')
+
+c1,c2=st.columns(2)
+if c1.button('Rotate left'):
+    Ri=open_image.rotate(90,expand=True)
+    st.image(Ri)
+if c1.button('Rotate Right'):
+    Ri=open_image.rotate(-90,expand=True)
+    st.image(Ri)
+if c1.button('Invert'):
+    Ri =open_image.rotate(180,expand=True)
+    st.image(Ri)
 
 
