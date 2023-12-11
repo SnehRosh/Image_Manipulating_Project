04669a76417e7eab3398be9239859b1d766c84b5
sneha <rosh3267@gmail.com>
2023-12-11 20:02:03 +0530

    Mod#3

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import streamlit as st
 import cv2     # image processing
 import numpy as np # numerical processing
 from PIL import Image # image processing
-from PIL import ImageDraw, ImageFont
+from PIL import ImageDraw, ImageFont, ImageFilter, ImageEnhance
 import matplotlib.pyplot as plt # plotting 
+
 
 # Config
 st.set_page_config(
@@ -11,7 +12,6 @@ st.set_page_config(
     page_title="Image Manipulation App",
     page_icon="📸",
     initial_sidebar_state='expanded',
-
 )
 
 with st.container():
@@ -30,40 +30,107 @@ with st.spinner('Processing Image'):
         #  let's display the image:
         st.image(open_image,caption='Original Image')
 
-
+actions = {}
 
 st.subheader('Resize the image',divider='blue')
 c1,c2=st.columns(2)
 #Resize
-if c1.button('Resize'): 
- ht = st.number_input('Enter the height of image: ',placeholder='Type a number',value=None)
- wd = st.number_input('Enter the width of image: ', placeholder='Type a Number',value=None)
- Resized=open_image.resize((ht,wd))
- c2.image(Resized)
+ 
+f = c1.form('resize')
+ht = f.number_input('Enter the height of image: ',placeholder='Type a number',value=0)
+wd = f.number_input('Enter the width of image: ', placeholder='Type a Number',value=0)
+btn = f.form_submit_button("Resize")
+if btn and isinstance(ht, (int,float)) and isinstance(wd, (int, float)):
+    Resized=open_image.resize((ht,wd))
+    c2.image(Resized)
+    Resized.save('Resized.png')
+    btn = st.download_button(
+            label="Download image",
+            data=open('Resized.png', 'rb').read(),
+            file_name="Resized_image.png",
+            mime="image/png"
+            )
+else:
+    c2.info("Please enter numeric values")
 
+
+    
 
 #Color change
 st.subheader('Convert the color of the image',divider='rainbow')
 
 c1,c2=st.columns(2)
-if c1.button('Gray'):
-        im=cv2.imread(uploaded_file)
-        cats_color_grey=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-        plt.imshow(cats_color_grey,cmap='gray')
-        plt.axis('off')
+filter_options = ['Outline','Inverted oulined','Emboss','Blur','Min_Filter','Sharpen','Smooth']
+filter = c1.radio("apply an image filter",filter_options)
+f_image = open_image
+if filter == filter_options[0]:
+    f_image = open_image.filter(ImageFilter.CONTOUR)
+if filter == filter_options[1]:
+    f_image = open_image.filter(ImageFilter.FIND_EDGES)
+if filter == filter_options[2]:
+    f_image = open_image.filter(ImageFilter.EMBOSS)
+if filter == filter_options[3]:
+    f_image = open_image.filter(ImageFilter.BLUR)   
+if filter == filter_options[4]:
+    f_image = open_image.filter(ImageFilter.MinFilter(3)) 
+if filter == filter_options[5]:
+    f_image = open_image.filter(ImageFilter.SHARPEN) 
+if filter == filter_options[6]:
+    f_image = open_image.filter(ImageFilter.SMOOTH)          
+c2.image(f_image)
+f_image.save('fimage.png')
+btn = st.download_button(
+    label="Download image",
+    data=open('fimage.png', 'rb').read(),
+    file_name="image_filter.png",
+    mime="image/png"
+    )
 
-
+#Rotate
 st.subheader('Rotate the image',divider='blue')
 
 c1,c2=st.columns(2)
-if c1.button('Rotate left'):
+
+rotate_options = ['Right','Left','Invert','Original']
+rotate = c1.radio("apply an image filter",rotate_options)
+if rotate==rotate_options[0]:
     Ri=open_image.rotate(90,expand=True)
-    st.image(Ri)
-if c1.button('Rotate Right'):
+    
+if rotate==rotate_options[1]:
     Ri=open_image.rotate(-90,expand=True)
-    st.image(Ri)
-if c1.button('Invert'):
+    
+if rotate==rotate_options[2]:
     Ri =open_image.rotate(180,expand=True)
-    st.image(Ri)
+    
+if rotate==rotate_options[3]:
+    Ri=open_image
+c2.image(Ri)
+Ri.save('Oriented.png')
+btn = st.download_button(
+    label="Download image",
+    data=open('Oriented.png', 'rb').read(),
+    file_name="Oriented_image.png",
+    mime="image/png"
+    )
+#crop
+st.subheader('Crop the image',divider='blue')
 
-
+c1,c2=st.columns(2)
+f = c1.form('Crop')
+lft = f.number_input('Left: ',placeholder='Type a number',value=0)
+upr = f.number_input('Upper: ',placeholder='Type a number',value=0)
+rgt = f.number_input('Right: ', placeholder='Type a Number',value=0)
+lwr = f.number_input('Lower: ', placeholder='Type a Number',value=0)
+btn = f.form_submit_button("Crop")
+if btn and isinstance(lft, (int,float)) and isinstance(rgt, (int, float)) and isinstance(upr, (int, float)) and isinstance(lwr, (int, float)):
+    Cropped=open_image.crop((lft,upr,rgt,lwr))
+    c2.image(Cropped)
+    Cropped.save('Cropped.png')
+    btn = st.download_button(
+            label="Download image",
+            data=open('Cropped.png', 'rb').read(),
+            file_name="Cropped_image.png",
+            mime="image/png"
+            )
+else:
+    c2.info("Please enter numeric values")
---
+++ readme.md
@@ -0,0 +1,5 @@
+![Alt text](Project_ss1.png)
+![Alt text](Project_ss2.png)
+![Alt text](Project_ss3.png)
+![Alt text](Project_ss4.png)
+![Alt text](Project_ss5.png)
